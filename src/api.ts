53d6c6822d8b5a3d4d5f00bfc561010cdfa660e9
unknown <mohamedmajilan@gmail.com>
2022-08-01 00:27:00 +0200

    bug fixing

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request";
 import { QueryClient } from "@tanstack/react-query";
 import { getSdk } from "./generated/graphql";
 
-const gqlClient = new GraphQLClient("http://localhost:3000/api/graphql");
+const gqlClient = new GraphQLClient("/api/graphql");
 export const { getDogs, getDog } = getSdk(gqlClient);
 export const queryClient = new QueryClient({
   defaultOptions: {
